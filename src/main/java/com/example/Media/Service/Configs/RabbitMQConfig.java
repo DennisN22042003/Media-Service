f19d7ab018bdfa070bcef5996def2c99a02a2eaf
DennisN22042003 <dennisnjuguna278@gmail.com>
2025-02-16 23:57:40 +0300

    Added RabbitMQ Dependencies, Configured RabbitMQ in application.properties, Defined RabbitMQ Queue, Exchange and Binding through RabbitMQConfig.java and created a RabbitMQ Producer through ImageRabbitMqProducer.java intended to all Media Service to notify Events Service when a user uploads an image which event the image belongs to

---
+++ src/main/java/com/example/Media/Service/Configs/RabbitMQConfig.java
@@ -0,0 +1,29 @@
+package com.example.Media.Service.Configs;
+
+import org.checkerframework.checker.units.qual.C;
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    
+    public static final String EXCHANGE_NAME = "event.image.exchange";
+    public static final String QUEUE_NAME = "event.image.queue";
+    public static final String ROUTING_KEY = "event.image.routingKey";
+
+    @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE_NAME, true);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    }
+}
---
+++ src/main/java/com/example/Media/Service/Services/ImageRabbitMqProducer.java
@@ -0,0 +1,26 @@
+package com.example.Media.Service.Services;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.Media.Service.Configs.RabbitMQConfig;
+import com.example.Media.Service.Models.ImageMetadata;
+
+@Service
+public class ImageRabbitMqProducer {
+    
+    private final RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    public ImageRabbitMqProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendImageEvent(String eventId, String imageUrl) {
+        // ImageEvent imageEvent = new ImageEvent(eventId, imageUrl);
+        ImageMetadata metadata = new ImageMetadata(eventId, imageUrl);
+        rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_NAME, RabbitMQConfig.ROUTING_KEY, metadata);
+        System.out.println("Send image event for eventId: " + eventId);
+    }
+}
