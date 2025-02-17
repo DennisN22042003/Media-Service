8bf427e5212c088a7a954d6205e6af589d43535a
DennisN22042003 <dennisnjuguna278@gmail.com>
2025-02-17 15:46:52 +0300

    Made the ImageEventDTO serializable so that the necessary metadata(eventId, imageUrl) can be sent in a manner that SimpleMessageConverter in RabbitMQ can understand.

--- src/main/java/com/example/Media/Service/Configs/RabbitMQConfig.java
+++ src/main/java/com/example/Media/Service/Configs/RabbitMQConfig.java
@@ -4,6 +4,8 @@ import org.checkerframework.checker.units.qual.C;
 import org.springframework.amqp.core.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 
 @Configuration
 public class RabbitMQConfig {
@@ -25,5 +27,12 @@ public class RabbitMQConfig {
     @Bean
     public Binding binding(Queue queue, DirectExchange exchange) {
         return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        // No need for a custom message convertor as long as DTO is Serializable
+        return rabbitTemplate;
     }
 }
--- src/main/java/com/example/Media/Service/DTO/ImageEventDTO.java
+++ src/main/java/com/example/Media/Service/DTO/ImageEventDTO.java
@@ -1,6 +1,10 @@
 package com.example.Media.Service.DTO;
 
-public class ImageEventDTO {
+import java.io.Serializable;
+
+public class ImageEventDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private String eventId;
     private String imageUrl;
 
